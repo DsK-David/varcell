688b63bacd4077dccf9dafba3d12cf1a4f6bd225
David silva <89320886+DsK-David@users.noreply.github.com>
2021-11-06 14:35:12 -0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function back(){
+           bio.style.background="#16B98E"
+           bio.style.color="black"
+           bio.style.border="4px solid #16B98E"
+
+}
+function back1(){
+        social.style.background="#1798C0"
+}
+function send(){
+        var but= document.getElementsById("but")
+        but.innerHTML=Enviado
+
+}
